5ddd1c7ddd7b88f2fd903bbcf3e6cfe4c4b8d619
Robrecht Siera <rob.developer.securemail@holoncom.eu>
2024-04-19 07:09:14 +0200

    beautify

--- src/lib/components/crudFormWithoutRouter.vue
+++ src/lib/components/crudFormWithoutRouter.vue
@@ -144,21 +144,21 @@ export default {
               id: this.id,
               language: this.language,
             })
-              .then((data) => {
-                  this.model = data;
-              })
-              .always(() => {
-                  this.loading = false;
-              });
+            .then((data) => {
+                this.model = data;
+            })
+            .always(() => {
+                this.loading = false;
+            });
         } else {
           this.connector
             .pService(this.resource, "get", { id: this.id })
-              .then((data) => {
-                  this.model = data;
-              })
-                .always(() => {
-                    this.loading = false;
-                });
+            .then((data) => {
+                this.model = data;
+            })
+            .always(() => {
+                this.loading = false;
+            });
         }
       }
     },
@@ -174,9 +174,9 @@ export default {
         data.entityType = this.entityType;
       }
       this.loading = true;
-        return this.connector.pService(this.resource, "create", data).then(() => {
-        
-        })
+        return this.connector
+        .pService(this.resource, "create", data)
+        .then(() => { })
         .always(() => {
             this.loading = false;
         });
@@ -184,9 +184,9 @@ export default {
     update(data) {
       this.loading = true;
       data.id = this.id; // TODO is this line necessary?
-        return this.connector.pService(this.resource, "update", data).then(() => {            
-            
-        })
+        return this.connector
+        .pService(this.resource, "update", data)
+        .then(() => { })
         .always(() => {
             this.loading = false;
         });
